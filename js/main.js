db1eddf7de731cf28c93a86f0f053e8e7a413c55
hmmdeif <hmmdeif@users.noreply.github.com>
2020-08-25 16:04:00 +0000

    deploy: 2291f662e22158b8e6a022e41b00d17b3f7875f1

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ bulmaCarousel.attach('#carousel', {
     infinite: true,
     autoplay: true
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+  
+    if ($navbarBurgers.length > 0) {
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+                const target = el.dataset.target
+                const $target = document.getElementById(target)
+                el.classList.toggle('is-active')
+                $target.classList.toggle('is-active')
+            })
+        })
+    }
+})
